67664a75dd15127f36eb537702ac913bb3872a5c
israel <israel.bejarano.a@gmail.com>
2020-04-01 10:53:36 +0200

    guardar mensajes

--- spring-boot-backend-chat/src/main/java/com/springboot/backend/chat/controllers/ChatController.java
+++ spring-boot-backend-chat/src/main/java/com/springboot/backend/chat/controllers/ChatController.java
@@ -3,16 +3,21 @@ package com.springboot.backend.chat.controllers;
 import java.util.Date;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.springboot.backend.chat.models.documents.Mensaje;
+import com.springboot.backend.chat.models.services.ChatService;
 
 @Controller
 public class ChatController {
 	
 	private String[] colores = {"red", "green", "blue", "magenta", "purple", "orange"};
+	
+	@Autowired
+	private ChatService chatService;
 	
 	@MessageMapping("/mensaje")
 	@SendTo("/chat/mensaje")
@@ -21,6 +26,8 @@ public class ChatController {
 		if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
 			mensaje.setColor(colores[new Random().nextInt(colores.length)]);
 			mensaje.setTexto("nuevo usuario");
+		} else {
+			chatService.guardar(mensaje);
 		}
 		return mensaje;
 	}
