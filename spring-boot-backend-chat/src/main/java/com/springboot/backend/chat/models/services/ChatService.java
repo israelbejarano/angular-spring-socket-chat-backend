98c8feb321e93d0598d66c5c4e405e205a09e637
israel <israel.bejarano.a@gmail.com>
2020-04-01 10:51:53 +0200

    Servicio para el chat

---
+++ spring-boot-backend-chat/src/main/java/com/springboot/backend/chat/models/services/ChatService.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.chat.models.services;
+
+import java.util.List;
+
+import com.springboot.backend.chat.models.documents.Mensaje;
+
+public interface ChatService {
+
+	public List<Mensaje> obtenerUltimos10Mensajes();
+
+	public Mensaje guardar(Mensaje mensaje);
+
+}
